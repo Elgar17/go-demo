81f9bdd0793c6d962a88d9cad809d8ae463e1820
Elgar17 <1653794708@qq.com>
2022-12-17 20:41:52 +0800

    feat: add hello-world-demo

---
+++ hello-world/README.md
@@ -0,0 +1,8 @@
+# hello-world
+
+在控制台输出 hello world 字符串。
+
+```bash
+# 运行
+go run hello.go
+```
---
+++ hello-world/main.go
@@ -0,0 +1,9 @@
+package main
+
+
+// fmt 是 go 官方提供的模块，包含输入输出方法
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!") // 在控制台输出 Hello World!
+}
